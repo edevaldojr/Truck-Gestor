9fee91ef83d61678b7c0e1867270b315f2d36fe8
EDEVALDO CHAVES JUNIOR <ed.tadsifpr@gmail.com>
2022-02-21 18:03:25 -0300

    Adicionando tela home com botoes

--- projeto-integrador/src/main/java/ifpr/pgua/eic/gestaocaminhao/App.java
+++ projeto-integrador/src/main/java/ifpr/pgua/eic/gestaocaminhao/App.java
@@ -5,8 +5,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
 import java.io.IOException;
+
+import ifpr.pgua.eic.gestaocaminhao.telas.Home;
 
 /**
  * JavaFX App
@@ -17,18 +20,26 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        Scene scene = new Scene(loadTela("fxml/home.fxml", o -> new Home()), 864, 515);
         stage.setScene(scene);
         stage.show();
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
+    public static Parent loadTela(String fxml, Callback controller) {
+        Parent root = null;
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(App.class.getResource(fxml));
+            loader.setControllerFactory(controller);
 
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
+            root = loader.load();
+
+        } catch (Exception e) {
+            System.out.println("Problema no arquivo fxml. Está correto?? " + fxml);
+            e.printStackTrace();
+            System.exit(0);
+        }
+        return root;
     }
 
     public static void main(String[] args) {
---
+++ projeto-integrador/src/main/java/ifpr/pgua/eic/gestaocaminhao/telas/Home.java
@@ -0,0 +1,26 @@
+package ifpr.pgua.eic.gestaocaminhao.telas;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Home {
+
+    @FXML
+    Button btHome;
+
+    @FXML
+    Button btCadastros;
+
+    @FXML
+    Button btEntradas;
+
+    @FXML
+    Button btDespesas;
+
+    @FXML
+    Button btRelatorios;
+
+    @FXML
+    Button btLogout;
+
+}
