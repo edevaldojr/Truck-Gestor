0a7b1cc8e964cd89abbc50260db3151c974e646b
Edevaldo Junior <ed.tadsifpr@gmail.com>
2022-03-13 21:18:48 -0300

    MONTANDO O BANCO DE DADOS E MELHORANDO O RELACIONAMENTO ENTRE TABELAS

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -1,16 +1,17 @@
 DROP TABLE IF EXISTS projeto_Usuario;
 CREATE TABLE `tads20_edevaldo`.`projeto_Usuario` (
-  `cpf` VARCHAR(14) NOT NULL,
-  `nome` VARCHAR(150) NOT NULL,
-  `cidade` VARCHAR(45) NOT NULL,
-  `endereco` VARCHAR(150) NOT NULL,
-  `telefone` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(150) NOT NULL,
-  `senha` VARCHAR(45) NOT NULL,
-  `gestor` TINYINT NULL,
-  `cnh` VARCHAR(14),
-  `ativo` TINYINT NOT NULL default 1,
-  PRIMARY KEY (`cpf`));
+      cpf VARCHAR(14) NOT NULL,
+      nome VARCHAR(150) NOT NULL,
+      telefone VARCHAR(45) NOT NULL,
+      email VARCHAR(150) NOT NULL,
+      senha VARCHAR(45) NOT NULL,
+      gestor TINYINT NULL,
+      cnh VARCHAR(14),
+      endereco_id int NOT NULL,
+      ativo TINYINT NOT NULL default 1,
+      PRIMARY KEY (cpf),
+	  FOREIGN KEY (endereco_id) REFERENCES projeto_Endereco(id)
+);
 
 create table projeto_Caminhao(
 	id int not null AUTO_INCREMENT,
@@ -22,4 +23,54 @@ create table projeto_Caminhao(
     tipo ENUM('GRANELEIRO', 'CONTAINER') not null,
     ativo TINYINT not null default 1,
     PRIMARY KEY(id)
+);
+
+create table projeto_Estado(
+	  id int not null AUTO_INCREMENT,
+    nome varchar(45) not null,
+    uf varchar(5) not null,
+    PRIMARY KEY(id)
+);
+
+create table projeto_Cidade(
+	  id int not null AUTO_INCREMENT,
+    nome varchar(45) not null,
+    id_estado int not null,
+    PRIMARY KEY(id),
+    FOREIGN KEY(id_estado) REFERENCES projeto_Estado(id)
+);
+
+drop table if exists projeto_Empresa;
+create table projeto_Empresa(
+	  id int not null AUTO_INCREMENT,
+    nome varchar(100) not null,
+    endereco int not null,
+    tipo enum ('DESTINO','ORIGEM') not null,
+    ativo TINYINT not null default 1,
+	  PRIMARY KEY(id)
+);
+
+create table projeto_Viagem(
+	  id int not null AUTO_INCREMENT,
+    peso double not null,
+    data_da_baixa date not null,
+    valor double not null,
+    empresa_origem_id int not null,
+    empresa_destino_id int not null,
+	  PRIMARY KEY(id),
+    FOREIGN KEY(empresa_origem_id) REFERENCES projeto_Empresa(id),
+	  FOREIGN KEY(empresa_destino_id) REFERENCES projeto_Empresa(id)
+);
+--Link para povoar cidades e estados
+--https://zerobugs.com.br/ver-post/codigo-sql-com-todas-as-cidades-e-estados-do-brasil-62/
+
+create table projeto_Endereco(
+	  id int not null AUTO_INCREMENT,
+    numero int not null,
+    complemento varchar(45) not null,
+    bairro varchar(45) not null,
+    cep varchar(10) not null,
+	  cidade_id int not null,
+    PRIMARY KEY(id),
+    FOREIGN KEY(cidade_id) REFERENCES projeto_Cidade(id)
 );
---
+++ projeto-integrador/src/main/java/ifpr/pgua/eic/gestaocaminhao/models/Endereco.java
@@ -0,0 +1,90 @@
+package ifpr.pgua.eic.gestaocaminhao.models;
+
+public class Endereco {
+
+    private int id;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cep;
+
+    private Cidade cidade;
+    private Viagem viagem;
+
+    public Endereco(int id, String numero, String complemento, String bairro, String cep,
+            Cidade cidade, Viagem viagem) {
+        this.id = id;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.cep = cep;
+        this.cidade = cidade;
+        this.viagem = viagem;
+    }
+
+    public Endereco() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public Cidade getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(Cidade cidade) {
+        this.cidade = cidade;
+    }
+
+    public Viagem getViagem() {
+        return viagem;
+    }
+
+    public void setViagem(Viagem viagem) {
+        this.viagem = viagem;
+    }
+
+    @Override
+    public String toString() {
+        return "Endereco [Cidade= " + cidade + ", Bairro= " + bairro + ", Cep= " + cep + ", Numero= " + numero + " ]";
+    }
+
+}
